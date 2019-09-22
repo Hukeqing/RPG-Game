01a0e66c7831b8dc30b99462d69b8f9292c19995
Mauve <keqing.hu@icloud.com>
2019-09-23 07:32:44 +0800

    1

---
+++ Assets/_My-RPGGame/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts.Player
+{
+    public class PlayerControl : MonoBehaviour
+    {
+        public float moveSpeed;
+
+        private Vector3 _newMove;
+
+        private void Update()
+        {
+            _newMove = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            transform.Translate(moveSpeed * Time.deltaTime * _newMove, Space.World);
+
+            // ReSharper disable once PossibleNullReferenceException
+            var cameraMoveRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(cameraMoveRay, out var hit, Mathf.Infinity))
+            {
+                transform.LookAt(hit.point);
+            }
+        }
+    }
+}
