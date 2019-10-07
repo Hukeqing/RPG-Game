7efdc70a079d1b4e777100d4ece53c6d6c12c795
HuKeqing <keqing.hu@icloud.com>
2019-10-07 09:26:50 +0800

    2

--- Assets/_My-RPGGame/Scripts/Player/PlayerControl.cs
+++ Assets/_My-RPGGame/Scripts/Player/PlayerControl.cs
@@ -7,19 +7,39 @@ namespace Scripts.Player
     {
         public float moveSpeed;
 
-        private Vector3 _newMove;
+        public bool IsDie { get; private set; }
+
+        private Vector3 newMove;
+        private Animator animator;
+        private static readonly int IsMoveHash = Animator.StringToHash("IsMove");
+        private static readonly int DieHash = Animator.StringToHash("Die");
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            IsDie = false;
+        }
 
         private void Update()
         {
-            _newMove = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            transform.Translate(moveSpeed * Time.deltaTime * _newMove, Space.World);
+            newMove = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            transform.Translate(moveSpeed * Time.deltaTime * newMove, Space.World);
+
+            animator.SetBool(IsMoveHash, newMove != Vector3.zero);
 
             // ReSharper disable once PossibleNullReferenceException
             var cameraMoveRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(cameraMoveRay, out var hit, Mathf.Infinity))
+            if (Physics.Raycast(cameraMoveRay, out var hit, Mathf.Infinity, 1 << 8))
             {
                 transform.LookAt(hit.point);
             }
+        }
+
+        public void Die()
+        {
+            if (IsDie) return;
+            IsDie = true;
+            animator.SetTrigger(DieHash);
         }
     }
 }
