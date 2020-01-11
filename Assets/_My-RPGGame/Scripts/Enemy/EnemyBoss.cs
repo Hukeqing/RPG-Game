faab979c8f2f93ffa05cb267e03ca10b645e8d05
HuKeqing <keqing.hu@icloud.com>
2020-01-12 01:51:43 +0800

    10

---
+++ Assets/_My-RPGGame/Scripts/Enemy/EnemyBoss.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Scripts.Enemy
+{
+    public class EnemyBoss : MonoBehaviour
+    {
+        public float bornCoolDown;
+        public GameObject[] enemyGameObject;
+        public Transform[] enemyBornPoint;
+
+        private float nextBorn;
+
+        private void Start()
+        {
+            nextBorn = Time.time + bornCoolDown;
+        }
+
+        private void Update()
+        {
+            if (nextBorn > Time.time)
+                return;
+            foreach (var tar in enemyBornPoint)
+            {
+                var curEnemy = Random.Range(0, enemyGameObject.Length);
+                Instantiate(enemyGameObject[curEnemy], tar);
+            }
+
+            nextBorn = Time.time + bornCoolDown;
+        }
+    }
+}
---
+++ Assets/_My-RPGGame/Scripts/Player/PlayerHurtEffect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.Player
+{
+    public class PlayerHurtEffect : MonoBehaviour
+    {
+        public Image hurtEffect;
+        public float hurtDeep = 0.3f;
+        public float hurtCoolDown = 1;
+
+        public void Hurt()
+        {
+            hurtEffect.color = new Color(1, 0, 0, hurtDeep);
+        }
+
+        private void Update()
+        {
+            if (hurtEffect.color.a > 0)
+            {
+//                hurtEffect.color = new Color(1, 0, 0, hurtEffect.color.a - hurtDeep / hurtCoolDown * Time.deltaTime);
+                hurtEffect.color = new Color(1, 0, 0, Mathf.Lerp(hurtEffect.color.a, 0, hurtCoolDown * Time.deltaTime));
+            }
+        }
+    }
+}
